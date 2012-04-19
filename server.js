755881acede36430b456d3bb619015fc242354bc
Justin <justin@nodesocket.com>
2012-04-19 01:09:33 -0700

    Version 0.1.0

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
+/**
+ * Quote Stream
+ *
+ * @version 0.1.0
+ * @date last modified 04/19/2012
+ * @author NodeSocket <http://www.nodesocket.com> <hello@nodesocket.com>
+ */
+
+ /*
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including 
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial 
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 ////
 // CONFIGURATION SETTINGS
 ///
 var PORT = 4000;
-var REFRESH_INTERVAL = 5000;
+var FETCH_INTERVAL = 5000;
 var PRETTY_PRINT_JSON = true;
 
 ///
@@ -69,5 +94,5 @@ io.sockets.on('connection', function(socket) {
 	//Every N seconds
 	setInterval(function() {
 		get_quote(socket)
-	}, REFRESH_INTERVAL);
+	}, FETCH_INTERVAL);
 });
