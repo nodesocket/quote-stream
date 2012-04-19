0bb9c0b51b01179ca8875eb88a99fc356b523fb2
Justin <justin@nodesocket.com>
2012-04-19 03:11:35 -0700

    Cosmetic changes.

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
  * Quote Stream
  *
  * @version 0.1.0
- * @date last modified 04/19/2012
  * @author NodeSocket <http://www.nodesocket.com> <hello@nodesocket.com>
  */
 
  /*
+ * Copyright (C) 2012 NodeSocket LLC 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
  * associated documentation files (the "Software"), to deal in the Software without restriction, including 
  * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
